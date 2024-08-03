73a7c94ad50b11fb3be689e96c1f185c7ba48230
kabetani-yusei <yuusei1771@gmail.com>
2024-08-03 23:14:49 +0900

    08/03 progress

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from typing import Union
-
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(q: Union[str, None] = Query(default=None, alias="item-query")):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+@app.get("/items/{item_id}")
+async def read_items(
+    *,
+    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+    q: str,
+    size: float = Query(gt=0, lt=10.5),
+):
+    results = {"item_id": item_id}
     if q:
         results.update({"q": q})
     return results
