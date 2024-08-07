df31041cc4e9397f5982ca0f66a0aeb7e4e16c62
kabetani-yusei <yuusei1771@gmail.com>
2024-08-07 22:14:42 +0900

    08/07 progress

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from typing import List, Set, Union
+from typing import List, Union
 
 from fastapi import FastAPI
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel
 
 app = FastAPI()
-
-
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
 
 
 class Item(BaseModel):
@@ -16,17 +11,10 @@ class Item(BaseModel):
     description: Union[str, None] = None
     price: float
     tax: Union[float, None] = None
-    tags: Set[str] = set()
-    images: Union[List[Image], None] = None
+    tags: List[str] = []
 
 
-class Offer(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    items: List[Item]
-
-
-@app.post("/offers/")
-async def create_offer(offer: Offer):
-    return offer
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
